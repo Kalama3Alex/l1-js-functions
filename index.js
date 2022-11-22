86ba2e89f5f199514bfa8c9ae4967e5e8094aaf9
Kalama3Alex <alexkalama58@gmail.com>
2022-11-22 12:11:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ function printValue(value){
 
 // 1.0 FUNCTION DECLARATION
 //TODO: create a basic function that adds two random values
+function add(){
+    const num1 = 6
+    const num2 = 10
+    printValue(num1 + num2)
+}
 
-
+add()
 //TODO: create a function that takes in two values and adds them
+function addNums(n1, n2){
+    printValue(n1 + n2)
+}
+addNums(6, 10)
 
 
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
-
-
+function addNumbers(n1, n2){
+    const sum = n1 + n2
+    return sum
+}
+const summation = addNumbers(45, 37)
+printValue(summation)
 // 1.0 FUNCTION EXPRESSION
 //TODO: create a basic function that adds two random values
-
+const addExp = function(){
+    printValue(20 + 4)
+}
+addExp()    
 
 //TODO: create a function that takes in two values and adds them
 
@@ -25,6 +41,12 @@ function printValue(value){
 
 // 1.0 ARROW FUNCTION
 //TODO: create a basic function that adds two random values
+const addAnonymous = () => {
+    printValue(60 + 90)
+}
+addAnonymous()
+
+
 
 
 //TODO: create a function that takes in two values and adds them
